666ff37f45ff9ddcade2cd4d038b2cbe10be3494
Легостин Иван <ivan.legostin@gazprombank.ru>
2022-10-20 11:00:14 +0600

    Find Pivot: Description

--- src/main/kotlin/leetcode/legostin/easy/FindPivot.kt
+++ src/main/kotlin/leetcode/legostin/easy/FindPivot.kt
@@ -1,5 +1,16 @@
 package leetcode.legostin.easy
 
+/**
+ * The pivot index is the index where the sum of all the numbers
+ * strictly to the left of the index is equal to the sum of all the numbers
+ * strictly to the index's right.
+ *
+ * If the index is on the left edge of the array, then the left sum is 0
+ * because there are no elements to the left.
+ * This also applies to the right edge of the array.
+ *
+ * Return the leftmost pivot index. If no such index exists, return -1
+ */
 class FindPivot {
 
     fun pivotIndex(nums: IntArray): Int {
