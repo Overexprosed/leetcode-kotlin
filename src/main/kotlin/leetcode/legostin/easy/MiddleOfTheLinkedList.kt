0a1d51ab2be5b8677bbfb66a6ab4e495cef23653
Легостин Иван <ivan.legostin@gazprombank.ru>
2022-10-21 10:10:38 +0600

    Middle of the linked list

---
+++ src/main/kotlin/leetcode/legostin/easy/MiddleOfTheLinkedList.kt
@@ -0,0 +1,24 @@
+package leetcode.legostin.easy
+
+import leetcode.legostin.easy.model.ListNode
+
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ *
+ * Each time, slow go 1 steps while fast go 2 steps.
+ * When fast arrives at the end, slow will arrive right in the middle.
+ */
+class MiddleOfTheLinkedList {
+
+    fun middleNode(head: ListNode?): ListNode? {
+        var slow = head
+        var fast = head
+
+        while (fast != null && fast.next != null) {
+            slow = slow!!.next
+            fast = fast.next!!.next
+        }
+        return slow
+    }
+}
