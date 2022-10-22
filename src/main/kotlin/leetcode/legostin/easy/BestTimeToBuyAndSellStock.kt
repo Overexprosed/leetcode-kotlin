84d09ffe9b393072d470fe0939d0f33bfc2cc4e9
Легостин Иван <ivan.legostin@gazprombank.ru>
2022-10-22 07:58:39 +0600

    Best time to buy and sell stock

---
+++ src/main/kotlin/leetcode/legostin/easy/BestTimeToBuyAndSellStock.kt
@@ -0,0 +1,23 @@
+package leetcode.legostin.easy
+
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction.
+ * If you cannot achieve any profit, return 0.
+ */
+class BestTimeToBuyAndSellStock {
+
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        var minPrice = Integer.MAX_VALUE
+        for (price in prices) {
+            minPrice = price.coerceAtMost(minPrice)
+            maxProfit = Math.max(maxProfit, price - minPrice)
+        }
+        return maxProfit
+    }
+}
