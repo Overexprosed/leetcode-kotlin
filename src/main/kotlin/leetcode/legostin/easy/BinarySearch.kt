a6fc6a655ce170cad34c84740915e22bce6f602a
Легостин Иван <ivan.legostin@gazprombank.ru>
2022-10-23 08:42:56 +0600

    Binary Search

---
+++ src/main/kotlin/leetcode/legostin/easy/BinarySearch.kt
@@ -0,0 +1,25 @@
+package leetcode.legostin.easy
+
+class BinarySearch {
+
+    fun search(nums: IntArray, target: Int): Int {
+        if (nums.isEmpty()) {
+            return -1
+        }
+        var high = nums.size - 1
+        var low = 0
+
+        while (low <= high) {
+            val middle = low + ((high - low) / 2)
+
+            if (nums[middle] < target) {
+                low = middle + 1
+            } else if (nums[middle] > target) {
+                high = middle - 1
+            } else {
+                return middle
+            }
+        }
+        return -1
+    }
+}
