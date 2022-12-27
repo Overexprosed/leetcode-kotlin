15d24ed1069c1401858f73b0665629a48e211762
Иван Легостин <ivan_legostin@mail.ru>
2022-12-27 14:09:01 +0300

    NumberComplement

---
+++ src/main/kotlin/leetcode/legostin/binary/NumberComplement.kt
@@ -0,0 +1,20 @@
+package leetcode.legostin.binary
+
+/**
+ * The complement of an integer is the integer you get when you flip all the 0's to 1's
+ * and all the 1's to 0's in its binary representation.
+ *
+ * For example, the integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+ * Given an integer num, return its complement.
+ */
+class NumberComplement {
+
+    fun findComplement(num: Int): Int {
+        // mask is all ones of num binary representation
+        var mask = 1
+        while (mask < num) {
+            mask = (mask shl 1) or 1
+        }
+        return num xor mask
+    }
+}
---
+++ src/test/kotlin/leetcode/legostin/binary/NumberComplementTest.kt
@@ -0,0 +1,33 @@
+package leetcode.legostin.binary
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class NumberComplementTest {
+
+    private val target = NumberComplement()
+
+    @Test
+    fun test1() {
+        val result = target.findComplement(5)
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun test2() {
+        val result = target.findComplement(1)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun test3() {
+        val result = target.findComplement(0)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun test4() {
+        val result = target.findComplement(122)
+        assertEquals(5, result)
+    }
+}
