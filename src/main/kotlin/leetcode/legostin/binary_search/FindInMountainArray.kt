f01d1a2d9c9b98a7eba569bf2b1190cf974783d6
Легостин Иван <ivan.legostin@gazprombank.ru>
2023-10-12 04:12:30 +0300

    FindInMountainArray

---
+++ src/main/kotlin/leetcode/legostin/binary_search/FindInMountainArray.kt
@@ -0,0 +1,58 @@
+package leetcode.legostin.binary_search
+
+@Suppress("DuplicatedCode")
+class FindInMountainArray {
+
+    fun findInMountainArray(target: Int, mountainArr: MountainArray): Int {
+        var left = 0
+        val length = mountainArr.length() - 1
+        var right = length
+        var peek = 0
+
+        // find index of peak
+        while (left < right) {
+            val middle = left + ((right - left) / 2)
+            if (mountainArr.get(middle) < mountainArr.get(middle + 1)) {
+                peek = middle + 1
+                left = peek
+            } else {
+                right = middle
+            }
+        }
+
+        // find target in the left of peak
+        left = 0
+        right = peek
+        while (left <= right) {
+            val middle = left + ((right - left) / 2)
+            if (target < mountainArr.get(middle)) {
+                right = middle - 1
+            } else if (target > mountainArr.get(middle)) {
+                left = middle +  1
+            } else {
+                return middle
+            }
+        }
+
+        // find target in the right of peak
+        left = peek
+        right = length
+        while (left <= right) {
+            val middle = left + ((right - left) / 2)
+            if (target < mountainArr.get(middle)) {
+                left = middle +  1
+            } else if (target > mountainArr.get(middle)) {
+                right = middle - 1
+            } else {
+                return middle
+            }
+        }
+
+        return -1
+    }
+}
+
+interface MountainArray {
+    fun get(index: Int): Int
+    fun length(): Int
+}
---
+++ src/test/kotlin/leetcode/legostin/binary_search/FindInMountainArrayTest.kt
@@ -0,0 +1,44 @@
+package leetcode.legostin.binary_search
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class FindInMountainArrayTest {
+
+    private val target = FindInMountainArray()
+
+    @Test
+    fun test1() {
+        val testMountainArray = TestMountainArray(array = intArrayOf(1, 2, 3, 4, 5, 3, 1))
+        val result = target.findInMountainArray(target = 3, testMountainArray)
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun test2() {
+        val testMountainArray = TestMountainArray(array = intArrayOf(0, 1, 2, 4, 2, 1))
+        val result = target.findInMountainArray(target = 3, testMountainArray)
+        assertThat(result).isEqualTo(-1)
+    }
+
+    @Test
+    fun test3() {
+        val testMountainArray = TestMountainArray(array = intArrayOf(1, 2, 3, 4, 5, 3, 1))
+        val result = target.findInMountainArray(target = 2, testMountainArray)
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun test4() {
+        val testMountainArray = TestMountainArray(array = intArrayOf(1, 5, 2))
+        val result = target.findInMountainArray(target = 2, testMountainArray)
+        assertThat(result).isEqualTo(2)
+    }
+
+    class TestMountainArray(
+        private val array: IntArray
+    ) : MountainArray {
+        override fun get(index: Int): Int = array[index]
+        override fun length(): Int = array.size
+    }
+}
