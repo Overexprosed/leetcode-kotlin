35db2bad3c2b38dfd6c69ecb42ed3a77ca6f0a3f
Иван Легостин <ivan_legostin@mail.ru>
2023-11-08 04:26:17 +0300

    DetermineIfCellIsReachableAtGivenTime

--- src/main/kotlin/leetcode/legostin/math/DetermineIfCellIsReachableAtGivenTime.kt
+++ src/main/kotlin/leetcode/legostin/math/DetermineIfCellIsReachableAtGivenTime.kt
@@ -4,6 +4,6 @@ class DetermineIfCellIsReachableAtGivenTime {
 
     fun isReachableAtTime(sx: Int, sy: Int, fx: Int, fy: Int, t: Int): Boolean {
         val need = Math.max(Math.abs(sx - fx), Math.abs(sy - fy))
-        return if (need > 0) t >= need else t != 1
+        return if (need > 0) t >= need else t != 1 // need == 0 && t != 1 - мы обязаны походить и промахнемся
     }
 }
