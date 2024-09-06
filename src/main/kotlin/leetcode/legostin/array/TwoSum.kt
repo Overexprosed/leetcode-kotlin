b68371b55069addd6fe738d1140a78bea8ff529a
Иван Легостин <ivan_legostin@mail.ru>
2024-09-06 06:25:04 +0300

    ContainsDuplicate: TwoSum: add comment for a solution

--- src/main/kotlin/leetcode/legostin/array/TwoSum.kt
+++ src/main/kotlin/leetcode/legostin/array/TwoSum.kt
@@ -7,14 +7,14 @@ class TwoSum {
         val hashMap = mutableMapOf<Int, Int>()
 
         for (i in nums.indices) {
-            val complement = target - nums[i]
-
+            // nums[i] in HashMap is complement, it means we found a solution
             if (hashMap.containsKey(nums[i])) {
                 result[0] = hashMap[nums[i]]!!
                 result[1] = i
                 return result
             }
-            hashMap[complement] = i
+
+            hashMap[target - nums[i]] = i
         }
         return result
     }
