0e082a35b0ad88074ac08059d4c8f1335aa90fd6
Иван Легостин <ivan_legostin@mail.ru>
2026-03-15 16:08:14 +0300

    GroupAnagrams honest implementation

--- src/main/kotlin/leetcode/legostin/array/GroupAnagrams.kt
+++ src/main/kotlin/leetcode/legostin/array/GroupAnagrams.kt
@@ -3,6 +3,23 @@ package leetcode.legostin.array
 class GroupAnagrams {
 
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        return strs.groupBy { it.toCharArray().sorted() }.map { it.value }
+        val map = mutableMapOf<Int, MutableList<String>>()
+
+        strs.forEach { str ->
+            val key = computeKey(str)
+            map.computeIfAbsent(key) { mutableListOf() }.add(str)
+        }
+
+        return map.values.toList()
+    }
+
+    private fun computeKey(str: String): Int {
+        val array = IntArray(26)
+
+        str.toCharArray().forEach { char ->
+            array[char - 'a'] = array[char - 'a'] + 1
+        }
+
+        return array.contentHashCode()
     }
 }
